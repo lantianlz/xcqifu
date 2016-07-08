65d33e5f479b2b23569fb2b45e43909a7ad0019e
stranger <286394973@qq.com>
2016-07-08 10:32:42 +0800

    模板

--- www/account/views.py
+++ www/account/views.py
@@ -15,14 +15,16 @@ from www.account import interface
 ub = interface.UserBase()
 
 
-def home_welcome(request, template_name='pc/index.html'):
+def home_welcome(request, template_name='mobile/index.html'):
     # print request.META.get("HTTP_USER_AGENT")
     return render_to_response(template_name, locals(), context_instance=RequestContext(request))
 
 
-def car_list(request, template_name='pc/car_list.html'):
+def supplier(request, template_name='mobile/supplier.html'):
     return render_to_response(template_name, locals(), context_instance=RequestContext(request))
 
+def supplier_detail(request, template_name='mobile/supplier_detail.html'):
+    return render_to_response(template_name, locals(), context_instance=RequestContext(request))
 
 def login(request, template_name='pc/account/login.html'):
     mobile = request.POST.get('mobile', '').strip()
