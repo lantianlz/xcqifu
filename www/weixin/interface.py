e73a8fe175675e65d88668f75b3537b7b2843274
simplejoy <lantian-lz@163.com>
2016-07-25 18:07:57 +0800

    独立出微信配置文件

--- www/weixin/interface.py
+++ www/weixin/interface.py
@@ -11,6 +11,7 @@ from pyquery import PyQuery as pq
 
 from common import cache, debug
 from www.misc import consts
+from www.weixin.weixin_config import dict_weixin_app
 
 
 dict_err = {
@@ -20,22 +21,6 @@ dict_err.update(consts.G_DICT_ERROR)
 
 
 weixin_api_url = 'https://api.weixin.qq.com'
-dict_weixin_app = {
-    'xcqifu_test': {
-        'app_id': 'wx0d227d4f9b19658a',
-        'app_secret': '513bdaf5b6022df4913f4cb5543fa688',
-        'app_type': 'gh_65671b9fff9d',
-        'token': 'xcqifu_test',
-        'url': ''
-    },
-    'xcqifu': {
-        'app_id': 'wx849eb5bab8d7e9e0',
-        'app_secret': '',
-        'app_type': 'gh_efd135bc6aa7',
-        'token': 'xcqifu',
-        'url': ''
-    },
-}
 
 
 class WeixinBase(object):
